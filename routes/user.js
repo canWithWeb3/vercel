a92260a13cc9ff616b14f7d8419ba0e278de15f3
newSoft5 <92645540+newsoft5@users.noreply.github.com>
2023-07-11 22:30:13 +0300

    node

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    return res.render("user");
+    return res.render("users/user");
 })
 
 module.exports = router;
